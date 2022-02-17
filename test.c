792e88d7779dc380244614dbbb259aa51eec4569
UH5555 <zhuhao162@163.com>
2022-02-17 21:53:55 +0800

    0217/函数第一节0-60min

---
+++ test.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//
+//}
+
+//#include<string.h>
+//int main()
+//{
+//	char arr1[] = "bit";
+//	char arr2[] = "######";
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	//strcpy - string copy - 字符串拷贝
+//	//strlen - string length - 字符串长度
+//	return 0;
+//}
+
+////memset - memory set - 内存设置
+//int main()
+//{
+//	char arr[] = "hello world";
+//	memset(arr, '*', 5);//前五个字符变更为*。
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+////定义函数
+// int get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = get_max(a, b);
+//	printf("max = %d\n", max);
+//	max = get_max(200, 100);
+//	printf("max = %d\n", max);
+//	return 0;
+//}
