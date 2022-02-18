d79797dfa02a6e2cf0e1f83f2c33c7b93393e18e
UH5555 <zhuhao162@163.com>
2022-02-18 19:26:20 +0800

    0218/函数第一节61-150min

--- test.c
+++ test.c
@@ -58,3 +58,64 @@
 //	printf("max = %d\n", max);
 //	return 0;
 //}
+
+////写一个函数交换两个整型变量的函数
+////Swap1为传值调用
+//void Swap1(int x, int y)//当实参传给形参的时候，形参其实是实参的一份临时拷贝。
+//{
+//	int tmp = 0;
+//	tmp = x;
+//	x = y;
+//	y = tmp;
+//}
+//
+////Swap2为传值调用
+//void Swap2(int* pa, int* pb)
+//{
+//	int tmp = *pa;//a地址的数据给tmp
+//	*pa = *pb;//b地址的数据给a地址的数据
+//	*pb = tmp;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("a=%d b=%d\n", a, b);
+//	//调用Swap函数
+//	Swap2(&a, &b);//取a，b的地址。
+//	/*Swap1(a, b);*/
+//	//int tmp = 0;
+//	//tmp = a;
+//	//a = b;
+//	//b = tmp;
+//	printf("a=%d b=%d\n", a, b);
+//
+//	return 0;
+//}
+
+//#include<math.h>
+////是素数时返回1，不是返回0
+//int is_prime(int n)
+//{
+//	int m = 2;
+//	for (m = 2; m <= sqrt(n); m++)
+//	{
+//		if (n % m == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	//打印100-200之间的素数
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断i是否为素数
+//		if (is_prime(i) == 1)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
