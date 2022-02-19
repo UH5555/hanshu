81e2ec99cae1a0d6263d1c34cd0dffc6d40b67d1
UH5555 <zhuhao162@163.com>
2022-02-19 23:17:03 +0800

    0219/函数第一节151-

--- test.c
+++ test.c
@@ -119,3 +119,134 @@
 //	}
 //	return 0;
 //}
+
+////判断一年是不是闰年
+//
+//int is_leap_year(int y)
+//{
+//	if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+//		return 1;
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		//判断year是否为闰年
+//		if (is_leap_year(year) == 1)
+//			printf("%d ",year);
+//	}
+//	return 0;
+//}
+
+////二分查找
+////在一个有序数列中	查找具体的某个数
+//
+//
+////                    本质arr是一个指针
+//int binary_search(int arr[], int k , int  sz)
+//{
+//	/*int sz = sizeof(arr) / sizeof(arr[0]);*/
+//	int left = 0;
+//	int right = sz - 1;
+//
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] < k)
+//			left = mid + 1;
+//		else if (arr[mid] > k)
+//			right = mid - 1;
+//		else
+//			return mid;
+//	}
+//	return -1;
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//                     传递过去的是数组首元素的地址
+//	int ret = binary_search(arr, k,sz);
+//	if (ret == -1)
+//		printf("找不到指定的数字\n");
+//	else
+//		printf("找到了，下标是:%d\n", ret);
+//	return 0;
+//}
+
+////写一个函数，每调用一次函数，num自增1
+//
+//void Add(int* p)
+//{
+//	(*p)++;
+//}
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	Add(&num);
+//	printf("num=%d\n", num);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int len = 0;
+//	//1
+//	len = strlen("abc");
+//	printf("%d\n", len);
+//	//2
+//	printf("%d\n", strlen("abc"));//链式
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d", 43)));//4321
+//	printf("%d\n", printf("%d\n", printf("%d\n", 43)));//43回车3回车2
+//	return 0;
+//}
+
+////函数声明
+//int Add(int x, int y);
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = 0;
+//	//函数调用
+//	sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//
+////函数的定义
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+
+
+#include "add.h"
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum = 0;
+	//函数调用
+	sum = Add(a, b);
+	printf("%d\n", sum);
+	return 0;
+}
+
